e67c81e347bc64da80876db9929a400e9328bcbd
toledui <luis_toledo35@hotmail.com>
2022-09-14 21:56:27 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MY FIRST WEBSITE WITH HTML5 CSS PHP AND MYSQL</title>
+</head>
+<body>
+    <p>this is my first website</p>
+    <?php
+        echo 'esto es php';
+    ?>
+
+</body>
+</html>
